9380a2823b952324bf559db1f30168a82f665576
R7yan <rayan.omer.5a207@gmail.com>
2025-09-21 16:25:17 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,3 @@
+document.getElementById("load-categories-btn").addEventListener("click", function() {
+  document.getElementById("categories").innerHTML = "<p>Categories will load here...</p>";
+});
